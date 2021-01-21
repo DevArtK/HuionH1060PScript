84efcce4822904d22a95923aa383c64781b42260
alpha0mgaak <alpha0megac0derpl@gmail.com>
2021-01-21 12:27:00 -0500

    Added button mapping fields

--- HuionSetup.sh
+++ HuionSetup.sh
@@ -21,3 +21,31 @@ xinput map-to-output "$STYLUS_ID" "$PRIMARY_SCREEN_ID"
 
 
 
+# Stylus Buttons
+STYLUS_NAME=$(xsetwacom --list | grep stylus | cut -d " " -f 1-5)
+# Uncomment and insert desired key 
+#xsetwacom --set "STYLUS_NAME" Button 1 "key "
+#xsetwacom --set "STYLUS_NAME" Button 2 "key "
+
+
+# Pad Buttons - Physical Buttons
+PAD_NAME=$(xsetwacom --list | grep "Pad pad" | cut -d " " -f 1-5)
+
+xsetwacom --set "$PAD_NAME" Button 1 "key " 
+xsetwacom --set "$PAD_NAME" Button 2 "key " 
+xsetwacom --set "$PAD_NAME" Button 3 "key " 
+xsetwacom --set "$PAD_NAME" Button 4 "key " 
+xsetwacom --set "$PAD_NAME" Button 5 "key " 
+xsetwacom --set "$PAD_NAME" Button 6 "key " 
+xsetwacom --set "$PAD_NAME" Button 7 "key " 
+xsetwacom --set "$PAD_NAME" Button 8 "key " 
+xsetwacom --set "$PAD_NAME" Button 9 "key " 
+xsetwacom --set "$PAD_NAME" Button 10 "key " 
+xsetwacom --set "$PAD_NAME" Button 11 "key " 
+xsetwacom --set "$PAD_NAME" Button 12 "key " 
+
+
+
+# Touch Pad Buttons --not yet functioning solution
+#TOUCH_PAD_NAME=$(xsetwacom --list | grep "Strip pad" | cut -f 1)
+
