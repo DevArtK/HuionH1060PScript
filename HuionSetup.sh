408b4a7b86b8ee855de9f5a0292fab1842387e46
alpha0mgaak <alpha0megac0derpl@gmail.com>
2021-01-21 22:07:34 -0500

    Added button remaps adjusted variable naming/parameter

--- HuionSetup.sh
+++ HuionSetup.sh
@@ -1,4 +1,4 @@
-#! usr/bin/bash
+#!/bin/bash
 
 STYLUS_ID=$(xsetwacom --list | grep stylus | cut -f 2 | cut -d " " -f 2)
 PAD_ID=$(xsetwacom --list | grep "Pad pad" | cut -f 2 | cut -d " " -f 2)
@@ -10,10 +10,10 @@ PRIMARY_SCREEN_ID=$(xrandr | grep primary | cut -d " " -f 1)
 #SECONDARY_SCREEN_ID=$(xrandr | grep HDMI-0 | cut -d " " -f 1)
 
 # Reset
-xsetwacom --set "$STYLUS_ID" ResetAreaq
+xsetwacom --set "$STYLUS_ID" ResetArea
 
 # Sensitivity(smoothness of strokes) -> Default 4, Range 1 - 20
-xsetwacom --set "$PAD_ID" RawSample 12
+xsetwacom --set "$STYLUS_ID" RawSample 12
 
 
 # Accept input from tablet to specified screen
@@ -31,21 +31,25 @@ STYLUS_NAME=$(xsetwacom --list | grep stylus | cut -d " " -f 1-5)
 # Pad Buttons - Physical Buttons
 PAD_NAME=$(xsetwacom --list | grep "Pad pad" | cut -d " " -f 1-5)
 
-xsetwacom --set "$PAD_NAME" Button 1 "key " 
-xsetwacom --set "$PAD_NAME" Button 2 "key " 
-xsetwacom --set "$PAD_NAME" Button 3 "key " 
-xsetwacom --set "$PAD_NAME" Button 4 "key " 
-xsetwacom --set "$PAD_NAME" Button 5 "key " 
-xsetwacom --set "$PAD_NAME" Button 6 "key " 
-xsetwacom --set "$PAD_NAME" Button 7 "key " 
-xsetwacom --set "$PAD_NAME" Button 8 "key " 
-xsetwacom --set "$PAD_NAME" Button 9 "key " 
-xsetwacom --set "$PAD_NAME" Button 10 "key " 
-xsetwacom --set "$PAD_NAME" Button 11 "key " 
-xsetwacom --set "$PAD_NAME" Button 12 "key " 
+# Issue with button id's not being numerical order - eh
+# Should be :
+#xsetwacom --set "$pad_name" button (1 - 12) "key (desired key)" 
+
+# Are :
 
 
+# First Button Grouping
+# --- Krita Mapping
+xsetwacom --set "$pad_name" Button 1 "key Insert" 
+xsetwacom --set "$pad_name" Button 2 "key Delete" 
+xsetwacom --set "$pad_name" Button 3 "key t" 
+xsetwacom --set "$pad_name" Button 8 "key Ctrl t" 
+xsetwacom --set "$pad_name" Button 9 "key Ctrl z" 
+xsetwacom --set "$pad_name" Button 10 "key e" 
 
-# Touch Pad Buttons --not yet functioning solution
-#TOUCH_PAD_NAME=$(xsetwacom --list | grep "Strip pad" | cut -f 1)
-
+# Second Button Grouping
+xsetwacom --set "$pad_name" Button 11 "key Shift" 
+xsetwacom --set "$pad_name" Button 12 "key b" 
+xsetwacom --set "$pad_name" Button 13 "key i" 
+xsetwacom --set "$pad_name" Button 14 "key o" 
+xsetwacom --set "$pad_name" Button 16 "key r" 
